c63e9794fb1f9b309d1a1ee616da38e591b657e4
Tamer-2020 <tamerabdeen93@gmail.com>
2025-02-14 19:19:27 -0500

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,7 +1,7 @@
 let length;
 let width;
 
-function calculateArea(){
+function calculateArea() {
     length = parseFloat (document.getElementById('length').value);
     width = parseFloat (document.getElementById('width').value);
 
@@ -9,3 +9,4 @@ function calculateArea(){
 }
 
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+
